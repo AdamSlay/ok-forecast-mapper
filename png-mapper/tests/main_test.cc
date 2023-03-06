8e6a7cea9ad40598608026c2687de4cc3d63d9a9
Adam Slay <aslay@mesonet.org>
2023-03-06 12:57:09 -0600

    google tests properly setup

--- png-mapper/tests/main_test.cc
+++ png-mapper/tests/main_test.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-//#include "../src/main.cpp"
+#include "../src/utils.h"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -8,6 +8,6 @@ EXPECT_STRNE("hello", "world");
 EXPECT_EQ(7 * 6, 42);
 }
 
-//TEST(MainTest, getX_zero) {
-//    EXPECT_EQ(getX(-180), 10);
-//}
+TEST(MainTest, getX_zero) {
+    EXPECT_EQ(getX(-180), -4850);
+}
